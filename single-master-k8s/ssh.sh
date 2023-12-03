af1f119682ab820c7699c1c9f7a6d602a7898817
douglas <ejrwls91@gmail.com>
2023-12-03 13:18:59 +0900

    update script syntax error

--- single-master-k8s/ssh.sh
+++ single-master-k8s/ssh.sh
@@ -9,7 +9,7 @@ systemctl restart sshd
 echo 'alias vi=vim' >> /etc/profile
 echo "sudo su -" >> .bashrc
 
-echo -e "192.168.56.161 k8s-master" >> /etc/hosts;done
+echo -e "192.168.56.161 k8s-master" >> /etc/hosts
 for i in {1..5};do nw=$((i+1));echo "192.168.56.16${nw} k8s-worker${i}" >> /etc/hosts;done
 
 echo -e "nameserver 168.126.63.1\nnameserver 8.8.8.8" > /etc/resolv.conf
