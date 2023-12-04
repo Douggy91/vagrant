9b0c18917437ff29177a85c38019bd2eae58e2c2
Douggy91 <ejrwls91@gmail.com>
2023-12-04 21:54:22 +0900

    update ssh-key

--- single-master-k8s/ssh.sh
+++ single-master-k8s/ssh.sh
@@ -10,9 +10,8 @@ echo 'alias vi=vim' >> /etc/profile
 echo "sudo su -" >> .bashrc
 
 sudo mkdir /root/.ssh
-SSH_KEY=`cat /root/.ssh/id_rsa.pub`
 sudo echo "\
-${SSH_KEY}
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDAGyqXvGQcHM6D8AKyuPT5rb6qBKtqXBtNO7bX9p9b9a6DAVOSyjW8VFcG45cM3thTxbz7qLUVyrEm23OVAddyO3ZK0Ljw7d7Pt2Go5u3VuCWn0l7k85PU7+wZyG57x+0z6B5WY5X6qWjyahFkFEmFxdM8YfyE6mrSJkuK10SCoVKwE+XM7B1fv6AXOk6JAT7/ZdRduic4ofqsceTu2ted5uo4MWzDuvd58tluMnctUJ3S9EbWrZR3oDvElIyIfwrqPcIRav56pwrWnmac/B8oQsk/VswNUsDNVEYT8XZ+EZXb8gFSig6NjiOiUliPxSR1Ppyfj29pZXg+P+k+3mdXKomSTFX3U/WVSi7qST77HQHOTTltV0lr57fp2Bo/iR6rBUd07rCSaqez8ocd7ZXzssb7CE7PiaGyFKpGfiRFC0LuhoUEmT9DRf5ClpgnzG6wmIBeBXEBV/0Xdkr0wx903l9LDd21TWKX6YibdgHOfKo/eh86d6Rihg2SSTNe5Ob2x/ymzAEkgYjOC5xDN/E57/hB5LD2Ft+GXgeUx6hlZVqvoh5uLFoKP8XqVAJB9W+lHbHM6HXF3hKLug75n031+RvUkFk4zvFUYfsRDIK3mtS1TEXKZc/UoA6dcAJhAmn41IedhhcyjPazoiuqXMgP3pJy0gm8Ip+cuT9zkUfR2Q== root@osc-doug\
 " > /root/.ssh/authorized_keys
 
 echo -e "192.168.31.161 k8s-master" >> /etc/hosts
